77b277cea7c583e9c1acae416d2d8e46914e5c98
Diogo Vianna V. Araujo <diogoviannaaraujo@gmail.com>
2023-04-25 20:32:49 -0300

    chore: base structure

---
+++ src/generate.c
@@ -0,0 +1,7 @@
+#include "xsmc.h"
+#include "xsHost.h"
+
+void xs_jwt_generate(xsMachine *the)
+{
+    modLog("xs_jwt_generate");
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+export default class {
+	static generate(message, private) @ "xs_jwt_generate";
+    static verify(token, public) @ "xs_jwt_verify";
+}
